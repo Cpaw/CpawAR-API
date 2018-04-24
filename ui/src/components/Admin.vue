54539afcfafdf110fd32f792b3c73f6f42d75fcc
palloc <palloc.mknet10@gmail.com>
2018-04-24 18:12:08 +0900

    add contents

--- ui/src/components/Admin.vue
+++ ui/src/components/Admin.vue
@@ -1,5 +1,11 @@
 <template>
   <article>
+    <section class="challenge">
+      <h2>Edit Challenge</h2>
+    </section>
+    <section class="user">
+      <h2>Edit User</h2>
+    </section>
   </article>
 </template>
 
