8f6320c3d3df3a02ff47e607d5fd7cf2e6ea54ec
palloc <palloc.mknet10@gmail.com>
2018-04-26 01:30:33 +0900

    add some

--- ui/src/components/admin/EditChallenge.vue
+++ ui/src/components/admin/EditChallenge.vue
@@ -1,6 +1,37 @@
 <template>
   <article v-if="isAdmin">
-    <section class="challenge">
+    <h2>問題追加</h2>
+    <section v-for="challenge in challenges">
+      <form v-on:submit.prevent="editChallenge">
+        <div class="field">
+          <div class="label">
+            <label for="title">Title:</label>
+          </div>
+          <div class="input">
+            <input class="title" type="text" name="title" v-model="challenge.Title">
+          </div>
+        </div>
+        <div class="field">
+          <div class="label">
+            <label for="contents">Challenge text:</label>
+          </div>
+          <div class="contents">
+            <textarea type="text" name="contents" v-model="challenge.QuestionText" cols="60" rows="5" />
+          </div>
+        </div>
+        <div class="field">
+          <div class="label">
+            <label for="weight">Weight:</label>
+          </div>
+          <div class="weight">
+            <input type="number" name="weight" v-model="challenge.Weight" />
+          </div>
+        </div>
+        <div class="submit">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div class="error" v-if="addChallengeError">Invalid data</div>
     </section>
   </article>
 </template>
